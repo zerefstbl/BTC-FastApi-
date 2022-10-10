20bc59eb64911b6097eefda15e691cf29d1aeafd
Eduardo de Oliveira <102335897+zerefstbl@users.noreply.github.com>
2022-10-09 21:26:46 -0300

    Delete connection.py

--- database/connection.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-
-from sqlalchemy.orm import sessionmaker
-
-url = 'mysql+asyncmy://root:dudu1234E@localhost:3306/fastapi'
-
-engine = create_async_engine(url)
-
-async_session = sessionmaker(engine, class_=AsyncSession)
